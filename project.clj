319846e6f965d0f834781f3b414b9a2807513b8f
Sinjin Swanepoel <sinjin.swanepoel@yuppiechef.com>
2017-12-14 14:03:13 +0200

    Pass in command line options and css as urls

--- project.clj
+++ project.clj
@@ -5,5 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [enlive "1.1.2"]
+                 [clj-http "3.7.0"]
                  [clj-time "0.6.0"]  
                  [org.apache.commons/commons-lang3 "3.1"]])
